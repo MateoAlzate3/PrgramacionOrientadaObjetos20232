eb5b0942be1f57a096410961d5344542da0d1b9b
MateoAlzate3 <mateoalzate07962@gmail.com>
2023-09-28 08:13:12 -0500

    Se reemplaza por codigo de profesor y despues se anexa lo que se vio en clase

--- src/edu/caldas/ingenieria/poo/gradebook/GradeBook.java
+++ src/edu/caldas/ingenieria/poo/gradebook/GradeBook.java
@@ -1,21 +1,65 @@
 package edu.caldas.ingenieria.poo.gradebook;
 
+import java.util.Scanner;
+
 public class GradeBook {
 	
-	private String courseName;
+private String courseName;
+	
+	//Zona de constructores
+	public GradeBook()
+	{
 		
+	}
+		/**
+	 * @param courseName
+	 */
+	public GradeBook(String courseName) {
+		this.courseName = courseName;
+	}
+
+	//Zonas getters y setters
+		/**
+	 * @return the courseName
+	 */
 	public String getCourseName() {
 		return courseName;
 	}
 
+	/**
+	 * @param courseName the courseName to set
+	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
 
-	
-	public void displayMessage(String course)
-	{
-		System.out.print(" hello from GadeBook class " + getCourseName());
+	//Zona de la logica de Negocio
+	public void displayMessage() {
+		System.out.println("Hello from GradeBook class " + getCourseName());
 	}
+	
+	public void determineClasssAverage() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int total = 0, grade, gradeCounter = 1, Average;
+		
+		while(gradeCounter <= 5) {
+			System.out.println("Please insert grade: ");
+			grade = sc.nextInt();
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+		}
+		
+		//promedio = sumatorio de muestras/# de muestras
+		Average = total / 5;
+		
+		System.out.println("The average of the grades is: " + Average);
+		
+		//int i = sc.nextInt();
+		
+	}
+	
+	// TODO Auto-generated constructor stub
 
 }
--- src/edu/caldas/ingenieria/poo/gradebook/GradeBookTest.java
+++ src/edu/caldas/ingenieria/poo/gradebook/GradeBookTest.java
@@ -3,23 +3,30 @@ package edu.caldas.ingenieria.poo.gradebook;
 import java.util.Scanner;
 
 public class GradeBookTest {
+
 	public GradeBookTest() {
-		
+		// TODO Auto-generated constructor stub
 	}
-	public static void main (String[] args )
-	{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
-		
+	    
 		GradeBook gradebook = new GradeBook();
-		GradeBook gradebookProgramacion00 = new GradeBook("Programacion Orientada a Objetos");
+		GradeBook gradebookProgramacionOO = new GradeBook("Programacion Orientadan a Objetos");
 		
-		System.out.print("give a course name: ");
+		//gradebook.determineClasssAverage();
+		gradebookProgramacionOO.determineClasssAverage();
+		
+		/*
+		System.out.println("Give a course name: ");
 		String courseName = sc.nextLine();
 		gradebook.setCourseName(courseName);
-		System.out.println("Nombre del curso desde el Main() : " + gradebook.getCourseName());
-		System.out.println("Nombre del curso conc constructor nombre" + gradebookProgramacion00);
-		gradebook.displayMessage(courseName);
-		
+		System.out.println("nombre del curso desde el Main() :" + gradebook.getCourseName());
+		System.out.println("nombre del curso con constructor nombre" + gradebookProgramacionOO.getCourseName());
+		gradebook.displayMessage();
+		*/
 	}
 
 }
